824c9779ae971d3907ad283f72868692cb9c9a42
Lindani Dlamini <40761107+Mleendox@users.noreply.github.com>
2024-02-04 22:25:45 +0200

    stokvel admin one

---
+++ lib/onboarding/reset_password.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+import 'package:stokvel_admin/controllers/app_controller.dart';
+import 'package:stokvel_admin/controllers/auth_controller.dart';
+import 'package:stokvel_admin/utils/theme_data.dart';
+import 'package:stokvel_admin/utils/utils.dart';
+
+class ResetPassword extends StatefulWidget {
+  const ResetPassword({super.key});
+
+  @override
+  State<ResetPassword> createState() => _ResetPasswordState();
+}
+
+class _ResetPasswordState extends State<ResetPassword>
+{
+  final _authController = AuthController.instance;
+  final _appController = AppController.instance;
+
+  final TextEditingController _validationCodeController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController = TextEditingController();
+  bool _isPasswordVisible = false;
+
+  bool _allFieldsFilled() {
+    if (_validationCodeController.text.trim().isEmpty) return false;
+    if (_passwordController.text.trim().isEmpty) return false;
+    return true;
+  }
+
+  bool _passwordsMatch() {
+    bool passwordsMatch = false;
+    if (_passwordController.text.trim() == _confirmPasswordController.text.trim()) {
+      passwordsMatch = true;
+    }
+    return passwordsMatch;
+  }
+
+  Future<void> _resetPasswordOnPressed() async
+  {
+    if (!_allFieldsFilled()) {
+      await showGetMessageDialog(
+        'Password reset info',
+        'Please fill in all fields.'
+      );
+
+      return;
+    }
+
+    if (!_passwordsMatch()) {
+      await showGetMessageDialog(
+        'Passwords info',
+        'Please ensure passwords are the same.'
+      );
+
+      return;
+    }
+
+    _authController.validatePasswordReset(
+      validationCode: _validationCodeController.text.trim(),
+      password: _passwordController.text.trim()
+    );
+  }
+
+  @override
+  Widget build(BuildContext context)
+  {    
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: offwhite_background,
+      ),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        color: offwhite_background,
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              spacer1(),
+              // greeting text
+              const Text(
+                'Setup your new password',
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              // user details form
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: Form(
+                  child: Column(
+                    children: [
+                      // CONFIRMATION CODE
+                      _appController.formDataField(
+                        fieldController: _validationCodeController,
+                        lableText: 'Confirmation code',
+                        textInputType: TextInputType.text
+                      ),
+                      // PASSWORD
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 5),
+                        child: Container(
+                          padding: const EdgeInsets.fromLTRB(12, 0, 12, 12),
+                          width: _appController.getWidgetWidth(),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10)
+                          ),
+                          child: TextFormField(
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {return 'Please enter some text';}
+                              return null;
+                            },
+                            controller: _passwordController,
+                            style: const TextStyle(
+                              fontSize: h3,
+                              color: dark_fonts_grey
+                            ),
+                            decoration: InputDecoration(
+                              suffixIcon: IconButton(
+                                icon: _isPasswordVisible
+                                      ? const Icon(Icons.visibility)
+                                      : const Icon(Icons.visibility_off),
+                                onPressed:() => setState(() => _isPasswordVisible = !_isPasswordVisible),
+                                color: Colors.redAccent,
+                              ),
+                              labelText: 'Password',
+                              labelStyle: const TextStyle(
+                                color: dark_fonts_grey
+                              ),
+                            ),
+                            obscureText: !_isPasswordVisible,
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ),
+              ),
+              spacer3(),
+              // RESET PASSWORD BUTTON
+              _appController.neuBox(
+                onTap: _resetPasswordOnPressed,
+                width: null,
+                child: const Center(
+                  child: Text(
+                    'Reset password',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: h3,
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 3
+                    ),
+                  ),
+                )
+              ),
+            ]
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/utils/utils.dart
@@ -0,0 +1,205 @@
+// CIRCULAR PROGRESS INDICATOR
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:stokvel_admin/utils/theme_data.dart';
+
+// double textfieldWidth = 310.0;
+const double h1 = 30.0;
+const double h2 = 22.0;
+const double h3 = 15.0;
+const double h4 = 12.0;
+
+Widget spacer1() => const SizedBox(height: 50.0);
+Widget spacer2() => const SizedBox(height: 30.0);
+Widget spacer3() => const SizedBox(height: 10.0);
+
+TextStyle contentTextStyle({
+  double fontSize = h3,
+  FontWeight fontWeight = FontWeight.normal,
+  Color fontColor = dark_fonts_grey}) {
+  return TextStyle(
+    fontSize: fontSize,
+    fontWeight: fontWeight,
+    color: fontColor
+  );
+}
+
+// APPBAR
+AppBar appBar() => AppBar(
+    elevation: 0.0,
+    backgroundColor: offwhite_background,
+  );
+  
+
+// CIRCULAR PROGRESS INDICATOR
+Future<void> showCircularProgressIndicator({required BuildContext context}) async => showDialog(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (context) {
+        return const AlertDialog(
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Center(
+                    child: CircularProgressIndicator(
+                      color: dark_fonts_grey,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Center(
+                    child: Text(
+                      'Please wait...',
+                      style: TextStyle(
+                        color: dark_fonts_grey,
+                        fontWeight: FontWeight.w600,
+                        fontSize: h2
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          backgroundColor: Colors.white,
+        );
+      },
+    );
+
+
+Future<void> getCircularProgressIndicator() async
+{
+  return Get.defaultDialog(
+    radius: 8.0,
+    backgroundColor: Colors.white,
+    barrierDismissible: false,
+    title: 'Please wait',
+    titleStyle: contentTextStyle(),
+    titlePadding: const EdgeInsets.all(10.0),
+    content: const SizedBox(
+      width: 300.0,
+      child: Center(
+        child: CircularProgressIndicator(
+          color: dark_fonts_grey,
+        ),
+      ),
+    ),
+    contentPadding: const EdgeInsets.all(20.0),
+  );
+}
+
+
+// G E T    D I A L O G S
+const double borderRadius = 8.0;
+Row _dialogHeading(String tittle) => Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      const Icon(
+        Icons.info_outline,
+        size: 30.0,
+      ),
+      const SizedBox(width: 10.0),
+      Text(
+        tittle,
+        style: contentTextStyle(),
+      ),
+    ],
+  );
+
+// ALERT DIALOG
+Future<void> showMyDialog(BuildContext buildContext, title, message) async => showDialog(
+      context: buildContext,
+      barrierDismissible: false, // user must tap button!
+      builder: (context) {
+        return AlertDialog(
+          icon: Text(
+            title,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: h1,
+              fontWeight: FontWeight.bold,
+              color: dark_fonts_grey
+            )
+          ),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: [
+                Text(
+                  message,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontSize: h3,
+                    color: dark_fonts_grey
+                  ),
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              child: const Text(
+                'Back',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: h2,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue
+                ),),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+          backgroundColor: Colors.white,
+        );
+      },
+    );
+
+
+Future<void> showGetMessageDialog(String tittle, String message)
+{
+  return Get.defaultDialog(
+    radius: borderRadius,
+    backgroundColor: Colors.white,
+    barrierDismissible: false,
+    title: '',
+    titlePadding: const EdgeInsets.all(0.0),
+    titleStyle: const TextStyle(fontSize: 0.0),
+    content: Column(
+      children: [
+        _dialogHeading(tittle),
+        const SizedBox(height: 20.0),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            message,
+            style: contentTextStyle()
+          ),
+        ),
+      ],
+    ),
+    contentPadding: const EdgeInsets.all(10.0),
+    cancel: Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          GestureDetector(
+            onTap: () => Get.back(),
+            child: Text(
+              'Back',
+              style: contentTextStyle(
+                fontWeight: FontWeight.bold,
+                fontColor: Colors.lightBlue
+              ),
+            ),
+          )
+        ],
+      )
+    ),
+  );
+}
+
